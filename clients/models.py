e6ca18635046fb15ce1b56273c86537e172ccafb
cyedanwaar <mojotwenty23@gmail.com>
2024-04-08 16:04:43 +0500

    update

--- clients/models.py
+++ clients/models.py
@@ -4,11 +4,12 @@ from django.db import models
 
 
 class Client(models.Model):
+    projectname = models.CharField(max_length=255)
+    solicitationnumber = models.IntegerField()
+    taskauthnumber = models.IntegerField()
+    
     name = models.CharField(max_length=255)
     phone = models.IntegerField()
     fax = models.IntegerField()
     email = models.EmailField()
     address = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
